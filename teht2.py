2edc3fcc60a8fe32f478b52ae67c6f4bf9cb883c
kangasjuhani <60339544+kangasjuhani@users.noreply.github.com>
2020-08-24 12:24:27 +0300

    Krisse

--- teht2.py
+++ teht2.py
@@ -4,15 +4,29 @@ naytto = pygame.display.set_mode((640, 400))
 pygame.display.set_caption("Piirtäminen")
 
 
-def main():
+#def main():
+ #   while True:
+  #      tapahtuma = pygame.event.poll()
+   #     if tapahtuma.type == pygame.QUIT:
+    #        break
+
+     #   naytto.fill((0, 0, 0))
+      #  kuva = pygame.image.load("pixil-frame-0.png").convert()
+       # naytto.blit(kuva, (200,200))
+        #pygame.display.flip()
+
+#main()
+
+kuva = pygame.image.load("pixil-frame-0.png").convert()
+def piirräkuva(kuva, x, y):
     while True:
-        tapahtuma = pygame.event.poll()
-        if tapahtuma.type == pygame.QUIT:
+      tapahtuma = pygame.event.poll()
+      if tapahtuma.type == pygame.QUIT:
             break
 
-        naytto.fill((0, 0, 0))
-        kuva = pygame.image.load("pixil-frame-0.png").convert()
-        naytto.blit(kuva, (200,200))
-        pygame.display.flip()
+      naytto.fill((0, 0, 0))
+      naytto.blit(kuva, (x,y))
+      pygame.display.flip()
 
-main()
+
+piirräkuva(kuva, 200, 200)

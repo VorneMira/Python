15165da08654b5c6df1fd3a821c6f9aed8472d4a
kangasjuhani <60339544+kangasjuhani@users.noreply.github.com>
2020-08-24 12:33:59 +0300

    Update teht2.py

--- teht2.py
+++ teht2.py
@@ -1,8 +1,12 @@
 import pygame
+import random
 
 naytto = pygame.display.set_mode((640, 400))
 pygame.display.set_caption("Piirtäminen")
 
+###################################################
+#####TEHT 1########################################
+###################################################
 
 #def main():
  #   while True:
@@ -11,22 +15,45 @@ pygame.display.set_caption("Piirtäminen")
     #        break
 
      #   naytto.fill((0, 0, 0))
-      #  kuva = pygame.image.load("pixil-frame-0.png").convert()
+      #  kuva = pygame.image.load("Krisse.png").convert()
        # naytto.blit(kuva, (200,200))
         #pygame.display.flip()
 
 #main()
 
-kuva = pygame.image.load("pixil-frame-0.png").convert()
-def piirräkuva(kuva, x, y):
-    while True:
-      tapahtuma = pygame.event.poll()
-      if tapahtuma.type == pygame.QUIT:
-            break
+###################################################
+#####TEHT 2########################################
+###################################################
 
-      naytto.fill((0, 0, 0))
-      naytto.blit(kuva, (x,y))
-      pygame.display.flip()
+#kuva = pygame.image.load("Krisse.png").convert()
+#def piirräkuva(kuva, x, y):
+    #while True:
+      #tapahtuma = pygame.event.poll()
+      #if tapahtuma.type == pygame.QUIT:
+            #break
+
+      #naytto.fill((0, 0, 0))
+      #naytto.blit(kuva, (x,y))
+      #pygame.display.flip()
 
 
-piirräkuva(kuva, 200, 200)
+#piirräkuva(kuva, 200, 200)
+
+###################################################
+#####TEHT 3########################################
+###################################################
+
+#kuva = pygame.image.load("Krisse.png").convert()
+#def piirräkuva(kuva):
+    #while True:
+      #tapahtuma = pygame.event.poll()
+      #if tapahtuma.type == pygame.QUIT:
+            #break
+      #x = random.randint(0,640)
+      #y = random.randint(0,400)
+      #naytto.fill((0, 0, 0))
+      #naytto.blit(kuva, (x,y))
+      #pygame.display.flip()
+
+
+#piirräkuva(kuva)
